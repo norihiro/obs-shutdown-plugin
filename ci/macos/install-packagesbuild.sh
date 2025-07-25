6873989a61b2e58b8a46884a25afe77b50701c80
Norihiro Kamae <norihiro@nagater.net>
2025-07-25 18:56:19 +0900

    ci: Fix sha256sum argument
    On macos-15 runner, it failed with a message below.
      usage: sha256sum [-bctwz] [files ...]
    
    Also adds a check to ensure the file was downloaded correctly.

--- ci/macos/install-packagesbuild.sh
+++ ci/macos/install-packagesbuild.sh
@@ -11,8 +11,12 @@ packages_hash='6afdd25386295974dad8f078b8f1e41cabebd08e72d970bf92f707c7e48b16c9'
 
 for ((retry=5; retry>0; retry--)); do
 	curl -o Packages.dmg $packages_url
-	sha256sum -c <<<"$packages_hash Packages.dmg" && break
+	sha256sum -c - <<<"$packages_hash Packages.dmg" && break
 done
+if ((retry == 0)); then
+	echo "Error: Failed to get Packages.dmg" >&2
+	exit 1
+fi
 
 hdiutil attach -noverify Packages.dmg
 packages_volume="$(hdiutil info -plist | grep '<string>/Volumes/Packages' | sed 's/.*<string>\(\/Volumes\/[^<]*\)<\/string>/\1/')"
